a68582d485cdfdfa2a5492848e0c4b3c72fe0085
Dao Vu Dat <dat.daovu@gmail.com>
2020-12-31 22:03:22 +0700

    Adding vets page and controller. Closes #7.

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package com.example.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+
+    @GetMapping(path = {"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+}
